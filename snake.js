226b0cfa6e1b73845e52f8c295e75a7ddfa81af3
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2016-10-06 12:46:48 +0300

    Refactored public methods definition

--- snake.js
+++ snake.js
@@ -10,31 +10,31 @@ function snake(start_length) {
     for (var i = 1; i < start_length; ++i) {
       this.body.push({x: i - 1, y: 0});
     }
-
-    this.draw = function(renderer) {
-        var length = this.body.length;
-        renderer.addSymbol('@', this.head.x, this.head.y);
-        for (var i = 0; i < length; ++i) {
-          renderer.addSymbol('o', this.body[i].x, this.body[i].y);
-        }
-    };
-
-    this.move = function(direction) {
-        if (direction != 'UP' && direction != 'LEFT' && direction != 'DOWN' &&
-            direction != 'RIGHT') {
-            return;
-        }
-
-        this.body.push({x: this.head.x, y: this.head.y});
-
-        switch (direction) {
-          case 'UP':    this.head.y -= 1; break;
-          case 'LEFT':  this.head.x -= 1; break;
-          case 'DOWN':  this.head.y += 1; break;
-          case 'RIGHT': this.head.x += 1; break;
-          default: break;
-        }
-
-        this.body.shift();
-    };
 }
+
+snake.prototype.draw = function(renderer) {
+    var length = this.body.length;
+    renderer.addSymbol('@', this.head.x, this.head.y);
+    for (var i = 0; i < length; ++i) {
+      renderer.addSymbol('o', this.body[i].x, this.body[i].y);
+    }
+};
+
+snake.prototype.move = function(direction) {
+    if (direction != 'UP' && direction != 'LEFT' && direction != 'DOWN' &&
+        direction != 'RIGHT') {
+        return;
+    }
+
+    this.body.push({x: this.head.x, y: this.head.y});
+
+    switch (direction) {
+      case 'UP':    this.head.y -= 1; break;
+      case 'LEFT':  this.head.x -= 1; break;
+      case 'DOWN':  this.head.y += 1; break;
+      case 'RIGHT': this.head.x += 1; break;
+      default: break;
+    }
+
+    this.body.shift();
+};
